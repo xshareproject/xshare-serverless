819a5cae300ce47cd8e96c62c0e97e1db323e006
pkbui <khang.bui@outlook.com>
2021-04-25 22:47:12 -0700

    Setup postgres with docker

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Project Dependancies
 		○ PostgreSQL
 		○ Sequelize ORM
 		○ To Docker
-		○ To install packages: run npm i on top level folder
 
 Development
 =============== 
-	- Backend:
-		○ cd server && npm run dev 
-		○ Or npm run backend
+	- Run npm install on root folder 
+	- To build docker backend image: Run docker build -t xshare-backend:latest . (the . is required, specifying current directory)
+	- To run container: docker-compose up
+	- If need to re-initialize database: Delete postgres container and volume, then run docker-compose up 
 =======
--- server/app.js
+++ server/app.js
@@ -19,8 +19,8 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 //Enable cors for local development and testing, disable this for deployment
 app.use(cors());
 app.get('/', (req, res) => res.send("Hello Swish"));
-app.use('/api/users', jsonParser, require('./routes/users'));
-app.use('/api/contacts', jsonParser, require('./routes/contacts'));
+// app.use('/api/users', jsonParser, require('./routes/users'));
+// app.use('/api/contacts', jsonParser, require('./routes/contacts'));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, console.log(`Server started on port ${PORT}`));
